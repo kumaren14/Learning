5877ba89abd0aba09d062f422dbeb6b27329254e
kumaren14 <kumaren.a@gmail.com>
2019-10-01 15:30:09 -0500

    I added some math onto here

--- Learning.py
+++ Learning.py
@@ -13,5 +13,12 @@ print("Kumaren Anand")
 
 print("this is fun")
 
-
 # hey we're going to do math now
+# use the print() command to solve equations
+# notice we removed the quotation in the parentheses\
+# time for basic math
+
+print(3*3)
+print(2-1)
+print(4+2+0+6+9)
+print(48/2)
