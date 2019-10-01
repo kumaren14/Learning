e0f7d60122510d4a56e99cc7025ff1eee1ee19c7
kumaren14 <kumaren.a@gmail.com>
2019-10-01 15:25:57 -0500

    I reformatted the code to fit our assignment

--- Learning.py
+++ Learning.py
@@ -1,4 +1,17 @@
+# "Hello"
+# " I'm an octotorne"
+# "With me you can take notes that don't affect your code"
+# "Let's have some fun
+
+# well these lines just print a few words right here
 print("hello world")
+
+# "Use the print("") command to make your computer say stuff
+
+# here are some more words that happen to be my name
 print("Kumaren Anand")
-print("this is a bit annoying")
-print("intersting")
+
+print("this is fun")
+
+
+# hey we're going to do math now
