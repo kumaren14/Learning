161d392c611f52fa897a659af82910b044d7a2d4
kumaren14 <kumaren.a@gmail.com>
2019-10-01 15:44:14 -0500

    Finished I figured out how to add floating point numbers

--- Learning.py
+++ Learning.py
@@ -17,8 +17,30 @@ print("this is fun")
 # use the print() command to solve equations
 # notice we removed the quotation in the parentheses\
 # time for basic math
+# we can also attribute a subject with the number
 
 print(3*3)
-print(2-1)
+
+# lets give a subject for the next equations
+
+print("Cats", 2-1)
+print("Dogs", 3+2)
+
 print(4+2+0+6+9)
 print(48/2)
+
+# Wait a minute, the last one seemed a bit weird. It ended in a decimal. That's called a floating point number.
+# you get that when you divide
+# we can get a floating point decimal on each of these by dividing by one
+
+print("Cats", 2-1 / 1)
+print("Dogs", 3+2 / 1)
+
+print(4+2+0+6+9 / 1)
+print(48/2 / 1)
+
+# pretty sweet, huh?
+
+# that's all for today! thanks for learning with me!
+
+print("Red flower with a green stem")
