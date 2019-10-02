e18993b506bdf456b443ba56bb6a1dd1e1849e96
kumaren14 <kumaren.a@gmail.com>
2019-10-02 15:47:15 -0500

    Finished Did some work with variables

--- Learning.py
+++ Learning.py
@@ -41,6 +41,25 @@ print(48/2 / 1)
 
 # pretty sweet, huh?
 
-# that's all for today! thanks for learning with me!
+# now for variables
+
+
+
+#variables
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+
+print("There are", cars, "cars available")
+print("There are only", drivers, "drivers available")
+print("There will be", cars_not_driven, "empty cars today")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today")
+print("We need to put about", average_passengers_per_car, " in each car.")
 
 print("Red flower with a green stem")
