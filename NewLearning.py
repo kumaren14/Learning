24a78266ed1ae8d7fdda6ccbb7f793d1bdf5c940
kumaren14 <kumaren.a@gmail.com>
2019-10-03 15:51:48 -0500

    I now present to you the only installment in the epic story of BOB THE DOG.

---
+++ NewLearning.py
@@ -0,0 +1,17 @@
+dogName = "Bob"
+dogHeight = 13
+dogLoyalty = "9000"
+dogLegs = 3.5000000
+
+# s is for strings (things with quotes)
+# d is for numbers
+# f adds a decimal
+
+
+print("The Story of Bob the Dog")
+print("By Kumaren Anand")
+print("I have a dog, his name is %s" % dogName)
+print("Bob is %d inches tall. He is a small dog." % dogHeight)
+print("Bob may not be a very smart dog, but he is very loyal. His loyalty level is over %s!" % dogLoyalty)
+print("Bob lost part of a leg in an accident, and he now only has %f legs. However, he is still a good dog." % dogLegs)
+
